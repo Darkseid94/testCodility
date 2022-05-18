3cb3886b907dce937e244ce672dc1c13a0b790dc
unknown <jesusv1994@outlook.com>
2022-05-18 05:45:46 -0500

    test

---
+++ menuHorizontal.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Test</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Tets</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+						<li class="separator hidden-lg"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ menuVertical.php
@@ -0,0 +1,43 @@
+<div class="sidebar" data-color="whi">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+    	<div class="sidebar-wrapper">
+            
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="index.php">
+                        <i class="pe-7s-home"></i>
+                        <p>Crop the Message</p>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="test2.php">
+                        <i class="pe-7s-shopbag"></i>
+                        <p>Shoe Factory</p>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="test3.php">
+                        <i class="pe-7s-network"></i>
+                        <p>TwoDigitHours</p>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="enysa.php">
+                        <i class="pe-7s-back-2"></i>
+                        <p>DeleteDefectiveRows</p>
+                    </a>
+                </li>
+              
+            
+                
+            </ul>
+    	</div>
+    </div>
